92a5ea5c33860ace6597d07c779a3fe90205b7ae
LIM JAEHO <58262954+hossi97@users.noreply.github.com>
2022-11-30 17:01:49 +0900

    fix: WepappApplication 코드 수정
    - 세미콜론 대신 콜론 작성으로 인한 컴파일 오류 수정

--- src/main/java/fingerorder/webapp/WebappApplication.java
+++ src/main/java/fingerorder/webapp/WebappApplication.java
@@ -1,7 +1,7 @@
 package fingerorder.webapp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication:
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebappApplication {
 
