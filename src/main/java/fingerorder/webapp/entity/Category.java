3116ab880a062fc85bc576d93b8dc938f21d2585
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2022-12-01 22:59:15 +0900

    Feature : 모든 엔티티 설계 및 작성 완료 (#7)
    * feat: 모든 엔티티 설계
    
    - 모든 엔티티 설계
    
    * 수정
    
    Co-authored-by: LIM JAEHO <58262954+hossi97@users.noreply.github.com>

---
+++ src/main/java/fingerorder/webapp/entity/Category.java
@@ -0,0 +1,32 @@
+package fingerorder.webapp.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+//    @OneToMany(mappedBy = "category")
+//    private List<Menu> menus = new ArrayList<>();
+
+}
