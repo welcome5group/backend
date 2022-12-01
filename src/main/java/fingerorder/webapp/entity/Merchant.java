87ab77a0bc38bd44b1e0be1bbc5b271e3ed08194
suzhanlee <wlscww@kakao.com>
2022-12-01 22:43:46 +0900

    feat: 모든 엔티티 설계
    - 모든 엔티티 설계

--- src/main/java/fingerorder/webapp/entity/Merchant.java
+++ src/main/java/fingerorder/webapp/entity/Merchant.java
@@ -1,28 +1,28 @@
 package fingerorder.webapp.entity;
-
-
 import fingerorder.webapp.status.UserStatus;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.Getter;
 
 @Entity
 @Getter
-public class Member {
+public class Merchant {
 
     @Id
-    @GeneratedValue
-    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "merchant_id")
     private Long id;
     private String email;
     private String password;
-    private String nickname;
     private UserStatus status;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private LocalDateTime deletedAt;
+    private String nickname;
+
 
 }
---
+++ src/main/java/fingerorder/webapp/status/UserStatus.java
@@ -0,0 +1,10 @@
+package fingerorder.webapp.status;
+
+public enum UserStatus {
+
+    //1. 활성화 상태
+    //2. 이메일 인증 X
+    //3. 삭제된 상태
+
+
+}
