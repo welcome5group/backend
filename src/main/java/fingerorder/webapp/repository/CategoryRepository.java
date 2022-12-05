154898019c8a1400ca79f6f2dd77d8a8deef9334
hossi97 <asd8500@naver.com>
2022-12-05 14:02:40 +0900

    feat: 간단한 수정 및 추가
    - 간단한 수정 및 추가

---
+++ src/main/java/fingerorder/webapp/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package fingerorder.webapp.repository;
+
+import fingerorder.webapp.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
