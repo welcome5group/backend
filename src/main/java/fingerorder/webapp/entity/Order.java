cdf420f3ceb4f01afc28890d1815308732319eb5
서형동(Seo HyungDong) <58661684+GaJaMy@users.noreply.github.com>
2022-12-08 22:48:06 +0900

    Feat/find password (#15)
    * feat: 회원 비밀 번호 초기화 기능 추가
    - 회원 비밀 번호 초기화 기능 추가
    - 회원 이메일 정보를 받아 8자리의 임의의 문자열을 생성
    - 8자리의 문자열을 암호화 하여 DB에 Update
    - 8자리의 암호화 되지 않은 문자열을 반환
    
    * test: 회원 비밀번호 초기화 테스트코드 작성
    - 회원 db에 저장시 비밀번호와 비밀번호 초기화 되었을때 리턴 문자열 비교
    
    * feat: smtp 이메일 사용하기 위한 dependency 추가 및 설정
    - smtp dependency 추가
    - mail 관련 설정
    
    * refactor: 비밀 번호 재설정으로 로직 리펙토링, 회원 서비스 리펙토링
    - UserStatus가 설정 되어 있지 않아 발생하던 문제로 회원 서비스 리펙토링
       - 회원 서비스 : 회원 가입, 로그인, 회원 정보 보기, 회원 정보 수정
    - 회원 정보 보기 request 형식 변경 RequestBody -> ModelAttribute
    - 회원 관련 DTO에 @Data 어노테이션 추가
    - 메일 관련 설정 추가
    - ROLE이 제대로 설정 되지 않던 현상 수정
        - hasRole(MEMBER) -> hasRole('MEMBER')
    
    * refactor: 비밀 번호 재설정으로 로직 리펙토링, 회원 서비스 리펙토링
     - UserStatus가 설정 되어 있지 않아 발생하던 문제로 회원 서비스 리펙토링
        - 회원 서비스 : 회원 가입, 로그인, 회원 정보 보기, 회원 정보 수정
    - 회원 정보 보기 request 형식 변경 RequestBody -> ModelAttribute
    - 회원 관련 DTO에 @Data 어노테이션 추가
    - 메일 관련 설정 추가 - ROLE이 제대로 설정 되지 않던 현상 수정
         - hasRole(MEMBER) -> hasRole('MEMBER')
    
    * fix: Order 예약어 사용으로 인한 DB 오작동
    - Order 테이블 명을 Orders로 변경

--- src/main/java/fingerorder/webapp/entity/Order.java
+++ src/main/java/fingerorder/webapp/entity/Order.java
@@ -1,36 +1,35 @@
 package fingerorder.webapp.entity;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import lombok.Getter;
 
 @Entity
 @Getter
+@Table(name = "orders")
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "order_id")
+    @Column(name = "orders_id")
     private Long id;
     private LocalDateTime createdAt;
     private int totalPrice;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "guest_id")
     private Guest guest;
 
