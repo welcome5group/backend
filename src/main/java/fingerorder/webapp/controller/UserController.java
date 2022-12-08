2a34451a47d2c165dacf91be4dd24f60f9377417
HyungDong <mansa0805@gmail.com>
2022-12-08 23:16:47 +0900

    fix: 회원 정보 수정 Method 변경 - PATCH -> PUT

--- src/main/java/fingerorder/webapp/controller/UserController.java
+++ src/main/java/fingerorder/webapp/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,7 +53,7 @@ public class UserController {
 		return ResponseEntity.ok(result);
 	}
 
-	@PatchMapping("/api/users")
+	@PutMapping("/api/users")
 	@PreAuthorize("hasRole('MEMBER') or hasRole('MERCHANT')")
 	public ResponseEntity<?> userInfoEdit(@RequestBody UserEditDto userEditDto) {
 		var result = this.userService.editUserInfo(userEditDto);
