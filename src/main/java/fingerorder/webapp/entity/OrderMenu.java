e975f91dc7aedf8afff06dc50fe45c0677380941
seongyeong826 <96734777+seongyeong826@users.noreply.github.com>
2022-12-12 23:03:11 +0900

    fix: 추가 안된 import 재추가

--- src/main/java/fingerorder/webapp/entity/OrderMenu.java
+++ src/main/java/fingerorder/webapp/entity/OrderMenu.java
@@ -10,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
