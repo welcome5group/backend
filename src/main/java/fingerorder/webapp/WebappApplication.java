62c8e2e5a7a7fb87c9a17b4e5ba54ca6a25c6061
hossi97 <asd8500@naver.com>
2022-12-15 17:29:16 +0900

    style: git pull origin feature to resolve conflict
    - git pull origin feature to resolve conflict

--- src/main/java/fingerorder/webapp/WebappApplication.java
+++ src/main/java/fingerorder/webapp/WebappApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebappApplication {
 
-
-    public static void main(String[] args) {
-        SpringApplication.run(WebappApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+	}
 
 }
