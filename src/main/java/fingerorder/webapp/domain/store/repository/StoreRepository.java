204f0cbf847d7f8b61c107188ba51f64a7e07c51
HyungDong <mansa0805@gmail.com>
2022-12-18 14:46:47 +0900

    style: 충동해결 위한 임시커밋

--- src/main/java/fingerorder/webapp/domain/store/repository/StoreRepository.java
+++ src/main/java/fingerorder/webapp/domain/store/repository/StoreRepository.java
@@ -7,19 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface StoreRepository extends JpaRepository<Store, Long> {
 
 
-    Store findByName(String name);
-
-//    @Query("select c from Store s join fetch s.categories c where s.id =: id")
-//    Optional<Category> findCategory(@Param("id") Long id);
-//
-//
-//    @Query("select c from Store s join fetch s.categories c")
-//    List<Category> findCategoriesV1(Long id);
-//
-//    @Query("select c from Store s join s.categories c")
-//    List<Category> findCategoriesV2(Long id);
-
-    List<Store> findAllByMemberId(Long id);
 
 
 }
