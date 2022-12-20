030eac2e3e39dd61ac3a74fe4d51a81413847d59
seongyeong826 <96734777+seongyeong826@users.noreply.github.com>
2022-12-21 05:02:19 +0900

    fix: merge 안된 내용 정리

--- src/main/java/fingerorder/webapp/domain/order/entity/OrderMenu.java
+++ src/main/java/fingerorder/webapp/domain/order/entity/OrderMenu.java
@@ -1,8 +1,6 @@
 package fingerorder.webapp.domain.order.entity;
 
 import fingerorder.webapp.domain.menu.entity.Menu;
-import fingerorder.webapp.domain.order.status.OrderStatus;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -11,29 +9,47 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderMenu {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "order_menu_id")
     private Long id;
-    private OrderStatus orderStatus;
-    private String name;
+
     private int count;
+
     private int totalPrice;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "orders_id")
     private Order order;
 
-
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "menu_id")
     private Menu menu;
 
+    private OrderMenu(Menu menu, int count) {
+        this.menu = menu;
+        this.count = count;
+        makeTotalPrice(menu.getPrice(), count);
+    }
+
+    public static OrderMenu createOrderMenu(Menu menu, int count) {
+        return new OrderMenu(menu, count);
+    }
+
+    public void addOrder(Order order) {
+        this.order = order;
+    }
+
+    private void makeTotalPrice(int price, int count) {
+        this.totalPrice = price * count;
+    }
 
 }
---
+++ src/main/java/fingerorder/webapp/domain/order/status/OrderStatus.java
@@ -0,0 +1,11 @@
+package fingerorder.webapp.domain.order.status;
+
+public enum OrderStatus {
+
+    //1. 완료
+    COMP,
+    //2. 미완료
+    INCOMP
+
+
+}
