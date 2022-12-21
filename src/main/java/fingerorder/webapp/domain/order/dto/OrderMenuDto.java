0ccfee2ebcd03364514e7e9ac6acf428d2bd5b3b
서형동(Seo HyungDong) <58661684+GaJaMy@users.noreply.github.com>
2022-12-21 20:58:14 +0900

    Feature (#37)
    * member 엔티티에 toMemberDto 메서드 수정
    
    * 이메일 인증 및 비밀번호 재설정 링크 주소 변경
    
    Co-authored-by: seongyeong826 <96734777+seongyeong826@users.noreply.github.com>
    Co-authored-by: suzhanlee <wlscww@kakao.com>
    Co-authored-by: suzhanlee <108535068+suzhanlee@users.noreply.github.com>
    Co-authored-by: LIM JAEHO <58262954+hossi97@users.noreply.github.com>

--- src/main/java/fingerorder/webapp/domain/order/dto/OrderMenuDto.java
+++ src/main/java/fingerorder/webapp/domain/order/dto/OrderMenuDto.java
@@ -1,13 +1,28 @@
 package fingerorder.webapp.domain.order.dto;
 
-import lombok.AllArgsConstructor;
+import fingerorder.webapp.domain.order.entity.OrderMenu;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderMenuDto {
 
-    private Long menuId;
+    private Long id;
+
     private int count;
+
+    private int totalPrice;
+
+    private OrderMenuDto(OrderMenu orderMenu) {
+        this.id = orderMenu.getId();
+        this.count = orderMenu.getCount();
+        this.totalPrice = orderMenu.getTotalPrice();
+    }
+
+    public static OrderMenuDto createOrderMenu(OrderMenu orderMenu) {
+        return new OrderMenuDto(orderMenu);
+    }
 
 }
--- src/main/java/fingerorder/webapp/domain/order/entity/Order.java
+++ src/main/java/fingerorder/webapp/domain/order/entity/Order.java
@@ -19,8 +19,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -39,7 +37,9 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "orders_id")
     private Long id;
+
     private int tableNum;
+
     private int totalPrice;
 
     @Enumerated(EnumType.STRING)
@@ -55,6 +55,10 @@ public class Order {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "store_id")
     private Store store;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderMenu> orderMenus = new ArrayList<>();
+
 
     private Order(Member member, Store store, List<OrderMenu> orderMenus, OrderStatus orderStatus) {
         this.member = member;
@@ -72,13 +76,11 @@ public class Order {
         return new Order(member, store, orderMenus, orderStatus);
     }
 
+
     private void addMenuItems(OrderMenu orderMenu) {
         orderMenu.addOrder(this);
         this.orderMenus.add(orderMenu);
     }
-
-    @OneToMany(mappedBy = "order")
-    private List<OrderMenu> orderMenus = new ArrayList<>();
 
     private void makeTotalPrice(int price) {
         this.totalPrice += price;
