b22f6f8b8e09fa98c578f454f2211a4a82d099c4
서형동(Seo HyungDong) <58661684+GaJaMy@users.noreply.github.com>
2022-12-21 17:51:19 +0900

    Feat/auth fix (#31)
    * feat: 매장 주문 내역 조회 기능 추가 및 버그 수정
    - 매장 주문 내역 조회 기능 알고리즘
        1. 매장 ID로 매장을 찾는다.
        2. 해당 매장에 관련된 주문들을 찾는다.
        3. OrderMenu에 대한 정보를 찾아 리턴
    - 버그 수정
        1. 회원 가입시 비밀번호 validation 체크시 자릿수를 체크하지 않던 현상 수정
    
    * fix: 일반 회원 가입시 nickName 중복을 체크하지 않던 현상 외 4
    1. 일반 회원 가입시 nickName 중복을 체크하지 않던 현상
        - db에서 nickName을 검색한 후 존재하면 error 응답
    2. 비밀번호 초기화시 올바른 비밀번호를 입력해도 진행되지 않던 현상
        - 비밀번호 validation 체크 로직 수정 하여 해결
    3. 비밀번호 재설정 이메일 보낼때 링크가 안열리던 현상
        - 올바른 링크 주소로 재설정하여 해결
    4. 올바른 메일 주소로 요청을 보내도 비밀번호 메일 보내기 기능이 작동하지 않던 현상
        - requestdto를 수정 하여 해결
    5. 카카오 로그인 기능 추가
    
    * fix: 카카오 로그인시 리다이렉트 되지 않던 현상 수정
    1. 올바르지 않은 리다이렉트 주소 응답으로 인한 버그
        - 올바른 리다이렉트 주소 응답으로 해결
    
    * fix: 비밀번호 수정 요청에 권한 삭제
    - uuid를 보내는 시점에서 토큰을 테워서 보낼 필요가 없음
    
    * fix: 인증 관련 쿠키 설정
    1. http only 쿠키에 refresh 토큰을 담아서 전달
    2. 토큰 만료 exception 추가
    3. 로그인시 accessToken만 전달하도록 수정
    4. 비밀 번호 재설정 기능을 모든 사용자로 변경
    
    * fix: 인증관련 수정
    1. 로그인시 access 토큰 만 전달하도록 변경
    2. refresh 토큰은 쿠키에 담아서 http only로 전달
    3. 비밀번호 재설정 모든 사용자 권한
    4. 토큰 만료 exception 추가
    
    * fix: 회원가입 이메일 인증 관련 수정
    1. 회원 가입시 이메일 인증 메일을 전달 하도록 수정
    2. 인증 확인 api 추가(/api/auth/sign-up/submit)
        - uuid를 받아 인증 처리
    3. user로 되어있던 Member 엔티티 함수 member로 수정
    4. 이메일 인증 관련 exception 추가
    5. 닉네임 변경시 중복된 닉네임이 있으면 exception 반환
    6. 회원 상태에 UNAUTHORIZED 추가
    
    * fix: 비밀번호 재설정 및 회원가입 인증 메일이 링크 작동 불능 현상
    - 링크를 다시 수정하여 해결
    
    * fix: 이메일 인증 관련 api 권한이 없어 접근이 안되던 현상
    - 모든 권한으로 접근 허용

--- src/main/java/fingerorder/webapp/domain/member/controller/TestController.java
+++ src/main/java/fingerorder/webapp/domain/member/controller/TestController.java
@@ -1,16 +1,10 @@
 package fingerorder.webapp.domain.member.controller;
 
-import fingerorder.webapp.domain.member.dto.TokenDto;
 import fingerorder.webapp.domain.member.service.UserService;
-import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequiredArgsConstructor
