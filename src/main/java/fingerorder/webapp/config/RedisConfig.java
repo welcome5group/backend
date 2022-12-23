e78f02657d30a0d75bee66791df8574f074d645c
HyungDong <mansa0805@gmail.com>
2022-12-23 19:57:04 +0900

    asdasda

--- src/main/java/fingerorder/webapp/config/RedisConfig.java
+++ src/main/java/fingerorder/webapp/config/RedisConfig.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
