d424fab775768f161ebe98b38c82f1c70917c8cc
서형동(Seo HyungDong) <58661684+GaJaMy@users.noreply.github.com>
2022-12-23 14:29:01 +0900

    Feature (#48)
    보안강화

---
+++ src/main/java/fingerorder/webapp/domain/store/dto/OrderDetailsRequestDto.java
@@ -0,0 +1,22 @@
+package fingerorder.webapp.domain.store.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetailsRequestDto {
+
+	Long storeId;
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	LocalDateTime startDate;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+
+	LocalDateTime endDate;
+}
