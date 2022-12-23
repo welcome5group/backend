13278486bc89ac01bdc0dfdae46bddcd59a205c6
HyungDong <mansa0805@gmail.com>
2022-12-23 19:30:31 +0900

    sdfsdf

--- src/main/java/fingerorder/webapp/config/RedisConfig.java
+++ src/main/java/fingerorder/webapp/config/RedisConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -13,6 +14,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @RequiredArgsConstructor
 @EnableRedisRepositories
 @Configuration
+@PropertySource("classpath:application-prod.yml")
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
