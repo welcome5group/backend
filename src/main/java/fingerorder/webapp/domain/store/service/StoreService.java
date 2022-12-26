eb522cf7b985e08bcee3060814c6c129218c4fd4
LIM JAEHO <58262954+hossi97@users.noreply.github.com>
2022-12-26 18:50:45 +0900

    Feat/cate (#64)
    * feat: 카테고리 코드 수정
    
    - Bean Validation 적용
    
    * feat: 카테고리 코드 수정
    
    - Bean Validation 적용
    - unique 제약조건에 대한 예외처리 방식 변경
    
    * feat: 매출내역, 주문내역 조회 요청데이터 변경
    
    - 매출내역, 주문내역 조회 요청데이터 변경
    - 리퀘스트 바디 -> 쿼리 파라미터로 변경
    
    * style: 코드 수정
    
    - 코드 수정
    
    * Update application.yml

--- src/main/java/fingerorder/webapp/domain/store/service/StoreService.java
+++ src/main/java/fingerorder/webapp/domain/store/service/StoreService.java
@@ -90,7 +90,7 @@ public class StoreService {
         return storeLookUpOrderResponse;
     }
 
-    public List<PaymentDetailsResponseDto> findSalesForMonth(
+    public List<PaymentDetailsResponseDto> findPaymentsForMonth(
         PaymentDatailsRequestDto paymentDatailsRequestDto) {
         List<PaymentDetailsResponseDto> orders = salesQueryRepository.findOrders(
             paymentDatailsRequestDto.getStoreId(), paymentDatailsRequestDto.getYear(), paymentDatailsRequestDto.getMonth());
