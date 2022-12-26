f6d7324c81daa62db52b6724c94c392e67fb3c0e
sunveloper <96734777+seongyeong826@users.noreply.github.com>
2022-12-27 00:46:39 +0900

    Feature (#81)
    * refactor : 메뉴 생성자 수정
    
    * Feat/fix add review (#80)
    
    * refactor : 메뉴 생성자 수정 (#79)
    
    * fix: 리뷰 등록 및 주문 등록 기능 수정
    
    - 주문 엔티티 수정
    - 리뷰 등록 시 리뷰 작성한 주문에 리뷰 작성 시도 시 에러처리
    - 점주가 회원의 리뷰 등록 시도 시
    에러처리
    - 리뷰 등록 시 주문의 리뷰상태 변경
    - 주문 등록 시 리뷰 상태 기본값 세팅
    
    Co-authored-by: suzhanlee <108535068+suzhanlee@users.noreply.github.com>
    
    Co-authored-by: suzhanlee <108535068+suzhanlee@users.noreply.github.com>

--- src/main/java/fingerorder/webapp/domain/review/exception/ReviewErrorCode.java
+++ src/main/java/fingerorder/webapp/domain/review/exception/ReviewErrorCode.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ReviewErrorCode implements ErrorCode {
 
-    NO_UNIQUE_REVIEW("R01", "이미 작성한 리뷰가 존재합니다.");
+    NO_UNIQUE_REVIEW("R01", "이미 작성한 리뷰가 존재합니다."),
+    NO_AUTH_REVIEW("R02", "손님만 작성할 수 있습니다.");
 
     private final String code;
     private final String message;
