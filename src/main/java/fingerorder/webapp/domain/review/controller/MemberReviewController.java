fd958b9e9e264adfed10b6a4f86018b02f3175fa
seongyeong826 <96734777+seongyeong826@users.noreply.github.com>
2022-12-26 14:15:24 +0900

    feat: 손님 리뷰 조회 기능 추가

--- src/main/java/fingerorder/webapp/domain/review/controller/MemberReviewController.java
+++ src/main/java/fingerorder/webapp/domain/review/controller/MemberReviewController.java
@@ -1,10 +1,14 @@
 package fingerorder.webapp.domain.review.controller;
 
+import fingerorder.webapp.domain.review.dto.MemberReviewResponse;
 import fingerorder.webapp.domain.review.dto.ReviewRequest;
 import fingerorder.webapp.domain.review.service.MemberReviewService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,5 +37,10 @@ public class MemberReviewController {
     public ResponseEntity<?> remove(@RequestBody ReviewRequest reviewRequest) {
         return memberReviewService.remove(reviewRequest);
     }
-    
+
+    @GetMapping("/{memberId}")
+    public List<MemberReviewResponse> find(@PathVariable("memberId") Long memberId) {
+        return memberReviewService.find(memberId);
+    }
+
 }
