9bc3d782c758fd76f42115f0ea1c90e6f99fe2f4
sunveloper <96734777+seongyeong826@users.noreply.github.com>
2022-12-26 21:59:01 +0900

    Feat/find incomp orders (#74)
    * docs: 주석 수정
    
    * feat: 주문상태 완료 처리 안된 주문목록 조회 기능 완료

--- src/main/java/fingerorder/webapp/domain/order/controller/OrderController.java
+++ src/main/java/fingerorder/webapp/domain/order/controller/OrderController.java
@@ -1,9 +1,14 @@
 package fingerorder.webapp.domain.order.controller;
 
+import fingerorder.webapp.domain.order.dto.GetIncompOrdersResponse;
 import fingerorder.webapp.domain.order.dto.SaveOrderRequest;
+import fingerorder.webapp.domain.order.entity.Order;
 import fingerorder.webapp.domain.order.service.OrderService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +24,10 @@ public class OrderController {
     @PostMapping("/order")
     public ResponseEntity<?> save(@RequestBody final SaveOrderRequest saveOrderRequest) {
         return orderService.save(saveOrderRequest);
+    }
+
+    @GetMapping("/incomp-orders/{storeId}")
+    public ResponseEntity<List<GetIncompOrdersResponse>> getIncompOrders(@PathVariable Long storeId) {
+        return ResponseEntity.ok(orderService.getIncompOrders(storeId));
     }
 }
