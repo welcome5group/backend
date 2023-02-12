6a812a59465719671ad43a3c7a310124c201bdcb
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-02-12 16:04:28 +0900

    - feat : 프로젝트 설정

---
+++ Desktop/beta/src/main/java/camping101/beta/BetaApplication.java
@@ -0,0 +1,13 @@
+package camping101.beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetaApplication.class, args);
+	}
+
+}
---
+++ Desktop/beta/src/test/java/camping101/beta/BetaApplicationTests.java
@@ -0,0 +1,13 @@
+package camping101.beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
